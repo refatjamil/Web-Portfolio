382da3ee40266ae6c7f5e473d1e0baeaa02cfa3f
Rifat Jamil <rifatjamil54@gmail.com>
2023-05-11 00:45:19 +0600

    added dark/light mode

--- project/views.py
+++ project/views.py
@@ -1,8 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
+
+
+
 def project(request):
+    if request.COOKIES.get('mode') == 'dark':
+        dark_mode = True
+    else:
+        dark_mode = False
+    context = {'pactive' : 'active', 'dark_mode': dark_mode}
+    return render(request,'project/project.html',context)
 
-    context = {'pactive' : 'active'}
-
-    return render(request,"project/project.html",context)
+def toggle_mode(request):
+    if request.COOKIES.get('mode') == 'dark':
+        response = redirect(request.META.get('HTTP_REFERER'))
+        response.set_cookie('mode', 'light')
+    else:
+        response = redirect(request.META.get('HTTP_REFERER'))
+        response.set_cookie('mode', 'dark')
+    return response
