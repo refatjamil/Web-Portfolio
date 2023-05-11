e022f9d68248adb46fe3c10da667401e8e0f73dc
Rifat Jamil <rifatjamil54@gmail.com>
2023-05-11 16:10:49 +0600

    python manage.py collectstatic

--- my_resume_project/settings.py
+++ my_resume_project/settings.py
@@ -132,7 +132,6 @@ STORAGES = {
     },
 }
 
-STATIC_ROOT = "/var/www/example.com/static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
